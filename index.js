b971a7b9e699ec7c5dcb3b2edb9cbb842d430a6f
saloxitdinovv <134138858+saloxitdinovv@users.noreply.github.com>
2023-06-21 18:10:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// let name = "Alex"
+// let account = 7777
+// let money = 10000
+// name = prompt('name pls').toLowerCase().trim()
+
+// if(name === 'alex') {
+//         account = +prompt('Введите номер счета')
+
+//     if (account === 7777) {
+//         money = +prompt('Сколько обналичить?')
+
+//         if (money < 10000) {
+//             alert('Все отлично')
+
+//             confirm(`Вы вывели ${money}`)
+//             confirm(`На вашем счету осталось ${10000 - +money}`)
+//         } else {
+//             alert('Недостаточно средств')
+//         }
+//     } else {
+//         alert('Аферист')
+//     }
+// } else {
+//     alert('Пользователь не найден, досвидули')
+// }
+
+
+
+// let name = prompt('Введите ваше имя')
+
+// if (name[0] === "A" || name[0] === "a") {
+//     let age = prompt('Сколько вам лет?')
+//     if (age >= 20 && age <= 40) {
+//         let money = prompt('Сколько у вас собой денег?')
+//         if (money >= 100) {
+//             let available_seats = 8
+//             let friends = prompt('Сколько вас?')
+//             if (friends <= available_seats) {
+//                 alert('Заходите!')
+//             } else {
+//                 alert(`К сожелению нет свободных мест. На данный момент есть ${available_seats} свободных мест`)
+//             }
+//         } else {
+//             alert('Сначала заработай потом приходи')
+//         }
+//     } else {
+//         alert('К сожелению вход запрещен')
+//     }
+// } else {
+//     alert('К сожелению вход запрещен')
+// }
+
